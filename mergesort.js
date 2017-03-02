2efd05887c27a1d4a70cb7d206e0ca0e02632c14
Jason <jason.miguel@gmail.com>
2017-03-02 14:51:13 -0500

    initialized mergesort

--- mergesort.js
+++ mergesort.js
@@ -1,15 +1,8 @@
-function bubbleSort(array){
-  var result = array;
-  debugger;
-  for (var i = 0; i < result.length; i++) {
-    for (var j = 0; j < result.length; j++) {
-      var first = result[j],
-      second = result[j + 1];
-      if (first > second) {
-        result[j] = second;
-        result[j + 1] = first;
-      }
-    }
-  }
-  return result
+function split(wholeArray) {
+
+  return [firstHalf, secondHalf]
+}
+
+function merge(array1, array2) {
+
 }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,8 +1,11 @@
-describe('Bubble Sort', function(){
-  it('handles an empty array', function(){
-    expect( bubbleSort([]) ).toEqual( [] );
+describe('Split Array function', function() {
+  it('is able to split an array into two halves', function () {
+    expect(split([3,4,6,1,2,5,7]).toEqual([3,4,6,1],[2,5,7]);
   });
-  it('sorts an array', function(){
-    expect( bubbleSort([5,4,3,1,2])).toEqual([1,2,3,4,5]);
-  })
+});
+
+describe('Merge Sort', function () {
+  it('is able to merge two sorted arrays into one sorted array', function() {
+    expect(merge([3,4,6,1],[2,5,7])).toEqual([1,2,3,4,5,6,7]);
+  });
 });
